395bd39e52b385b6f35b9d8abcc1b799f82aa05f
Miles Ryder <miller1405@tamu.edu>
2025-02-19 15:26:51 -0600

    JDB Demo (GUI to DB)

---
+++ jdbc_demo/jdbc_demo/dbSetup.java
@@ -0,0 +1,9 @@
+/*
+CSCE 315
+Database setup example
+Purpose: Keeping usernames and password private.
+*/
+public final class dbSetup {
+  public static final String user = "team_phoenix";
+  public static final String pswd = "123";
+}// end class
---
+++ jdbc_demo/jdbc_demo/jdbcpostgreSQLGUI.java
@@ -0,0 +1,54 @@
+import java.sql.*;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+
+//import java.sql.DriverManager;
+/*
+CSCE 315
+9-25-2019
+ */
+public class jdbcpostgreSQLGUI {
+  public static void main(String args[]) {
+    dbSetup my = new dbSetup();
+    // Building the connection
+    Connection conn = null;
+    try {
+      Class.forName("org.postgresql.Driver");
+      conn = DriverManager.getConnection("jdbc:postgresql://csce-315-db.engr.tamu.edu/team_phoenix_db",
+          my.user, my.pswd);
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.err.println(e.getClass().getName() + ": " + e.getMessage());
+      System.exit(0);
+    } // end try catch
+    JOptionPane.showMessageDialog(null, "Opened database successfully");
+    String cus_lname = "";
+    try {
+      // create a statement object
+      Statement stmt = conn.createStatement();
+      // create an SQL statement
+      String sqlStatement = "SELECT first_name FROM employees";
+      // send statement to DBMS
+      ResultSet result = stmt.executeQuery(sqlStatement);
+
+      // OUTPUT
+      JOptionPane.showMessageDialog(null, "Employee First names from the Database.");
+
+      // System.out.println("______________________________________");
+      while (result.next()) {
+        // System.out.println(result.getString("cus_lname"));
+        cus_lname += result.getString("first_name") + "\n";
+      }
+    } catch (Exception e) {
+      JOptionPane.showMessageDialog(null, "Error accessing Database.");
+    }
+    JOptionPane.showMessageDialog(null, cus_lname);
+    // closing the connection
+    try {
+      conn.close();
+      JOptionPane.showMessageDialog(null, "Connection Closed.");
+    } catch (Exception e) {
+      JOptionPane.showMessageDialog(null, "Connection NOT Closed.");
+    } // end try catch
+  }// end main
+}// end Class
